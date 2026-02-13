eba3a8dedff254c264763195cd4714c47eac4149
thetosinfaith <hellotosinfaith@gmail.com>
2026-02-13 20:09:11 +0100

    added my letter code

--- app.js
+++ app.js
@@ -7,4 +7,25 @@ function toggleMessage() {
     }
 }
 
+function runAway() {
+    const btn = document.getElementById('noBtn');
+    
+    // Switch to absolute so it can fly anywhere
+    btn.style.position = 'absolute';
 
+    // Calculate a random spot on the screen
+    // We subtract 150 so it doesn't go off the edge
+    const newX = Math.random() * (window.innerWidth - 150);
+    const newY = Math.random() * (window.innerHeight - 150);
+
+    btn.style.left = newX + 'px';
+    btn.style.top = newY + 'px';
+}
+
+function showLove() {
+  document.getElementById('successOverlay').style.display = 'flex';
+}
+
+function closeOverlay() {
+  document.getElementById('successOverlay').style.display = 'none';
+}
